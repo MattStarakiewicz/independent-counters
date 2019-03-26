e886dcf34594b543488184fdd6467a06635cd66d
Mateusz Starakiewicz <ms.starakiewicz@gmail.com>
2019-03-26 21:51:52 +0100

    add next counters

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var Counter1 = React.createClass({
+var Counter = React.createClass({
     getInitialState: function() {
         return {
             counter: 0
@@ -17,45 +17,22 @@ var Counter1 = React.createClass({
         });
     },
 
-    render: function() {
-        return React.createElement('div', {className: 'counterBox2'},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter + ' '),
-            React.createElement('button', {id: 'in-btn', type: 'button', onClick: this.increment}, "Dodaj" + ' '),
-            React.createElement('button', {id: 'dec-btn', type: 'button', onClick: this.decrement}, "Odejmij" + ' ')
-        );
-    }
-});
-
-var element1 = React.createElement(Counter1);
-ReactDOM.render(element1, document.getElementById('app'));
-
-var Counter2 = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-
-    decrement: function() {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
+    
 
     render: function() {
-        return React.createElement('div', {className: 'counterBox2'},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter + ' '),
-            React.createElement('button', {id: 'in-btn', type: 'button', onClick: this.increment}, "Dodaj" + ' '),
-            React.createElement('button', {id: 'dec-btn', type: 'button', onClick: this.decrement}, "Odejmij" + ' ')
+        return React.createElement('div', {className: 'counterBox'},
+                React.createElement('span', {}, 'Licznik ' + this.state.counter + ' '),
+                React.createElement('button', {id: 'in-btn', type: 'button', onClick: this.increment}, "Dodaj" + ' '),
+                React.createElement('button', {id: 'dec-btn', type: 'button', onClick: this.decrement}, "Odejmij" + ' '),
         );
     }
+
+
 });
 
-var element2 = React.createElement(Counter2);
-ReactDOM.render(element2, document.getElementById('app2'));
+var element = React.createElement("div", {},
+    React.createElement(Counter),
+    React.createElement(Counter),
+    React.createElement(Counter)
+);
+ReactDOM.render(element, document.getElementById('app'));
